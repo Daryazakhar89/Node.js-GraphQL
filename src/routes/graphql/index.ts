d537fed1b9bd794fcfe4656272245f071bc543f5
Darya Zakhar <daryazakhar.89@gmail.com>
2023-07-22 00:17:11 +0400

    feat: implement limit the complexity of the graphql queries (all tests passed)

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+
 import { createGqlResponseSchema, gqlResponseSchema, graphQlSchema } from './schemas.js';
-import { graphql } from 'graphql';
 import { resolvers } from './resolvers.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -15,6 +17,15 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req, reply) {
       const { query, variables } = req.body;
+
+      const limitTheComplexity = 5;
+      const errors = validate(graphQlSchema, parse(req.body.query), [depthLimit(limitTheComplexity)]);
+
+      if (errors.length) {
+        return {
+          errors,
+        };
+      }
 
       const result = await graphql({
         schema: graphQlSchema,
